aba60ae05e795372c221210abec1b24ef66aa70e
Mdzahin <afsarzahin@gmail.com>
2020-08-06 16:29:49 +0600

    final commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const app = require('express')();
 const axios = require('axios')
 
-app.post('/', (req, res) => {
-    var random = Math.floor(Math.random() * 100)
+app.post('/deploy', (req, res) => {
+    var random = Math.floor(Math.random() * 100);
     if (random > 70) {
         axios.post('https://api.netlify.com/build_hooks/5f2bd48fdbfb5e93a1458b0e')
     }
+    res.send("ok  " + random)
+})
+app.post('/', (req, res) => {
+    res.send("ok")
+})
+app.get('/', (req, res) => {
     res.send("ok")
 })
 

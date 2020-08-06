81941782cb33ade1ee7b9ba2edbedabf4bb0898c
Mdzahin <afsarzahin@gmail.com>
2020-08-06 16:47:11 +0600

    final commit 3

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.get('/', (req, res) => {
     res.send("ok")
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
-app.listen(port)
+app.listen(port, () => {
+    console.log("server running on port " + port)
+})
